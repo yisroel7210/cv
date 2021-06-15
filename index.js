5abf398917660ace459f66b400587cbd65939876
yisroel7210 <81200488+yisroel7210@users.noreply.github.com>
2021-06-15 17:31:25 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-var  new = document.li;
-new.
+alert("it worked");
+alert("ready for the next?");
+document.querySelector('a').style.backgroundColor = "yellow";
+//document.querySelector('button').style.backgroundColor = "black";
+document.querySelectorAll('td')[0].classList.add("huge");
+document.querySelector('h3').innerHTML = "Good Working Places";
+//document.querySelector('h5'  'a').innerHTML = "<em>Back</em>";
+document.querySelector('h5').classList.add("huge");
